c10086757fb68a7a9741c598baa0f628fb100e78
Harsh <harsh912823@gmail.com>
2026-01-07 19:44:58 +0530

    add some console.log for testing

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ import { connectDB } from "./config/database.js";
 import userRouter from "./routes/user.js";
 import messageRouter from "./routes/message.js";
 
+const PORT_NO = process.env.PORT_NO || 5000;
+
 import { app, server, allowedOrigins } from "./lib/socket.js";
 
 app.use(
@@ -30,16 +32,17 @@ app.use("/api/status", (req, res) => res.send("Server is live"));
 app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
 
-const PORT_NO = process.env.PORT_NO || 5000;
-
 connectDB()
   .then(() => {
+    console.log("Database connection established..");
     if (process.env.NODE_ENV !== "production") {
       server.listen(PORT_NO, () => {
         console.log(`Server running on port ${PORT_NO}`);
       });
     }
   })
-  .catch((err) => console.log(err));
+  .catch((err) =>
+    console.log("Database not connected! --> Error: " + err.message)
+  );
 
 export default server;
